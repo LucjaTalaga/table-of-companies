4862e8bc86b36afe85d65941d39f77320426524c
Piotr Talaga <piotrek.talaga@gmail.com>
2020-04-19 10:57:22 +0200

    Updates

---
+++ precache-manifest.47e1c67f0c91805bfa5ce22af330fbf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95640a10bd300060514c72469b927aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c87508b0c4d4a2f7129",
+    "url": "/static/css/main.f1776eab.chunk.css"
+  },
+  {
+    "revision": "600758a7d27681d94950",
+    "url": "/static/js/2.43f78f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43f78f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c87508b0c4d4a2f7129",
+    "url": "/static/js/main.4a890965.chunk.js"
+  },
+  {
+    "revision": "1a4b259826ce8e3e9453",
+    "url": "/static/js/runtime-main.fb5fa7d3.js"
+  }
+]);
